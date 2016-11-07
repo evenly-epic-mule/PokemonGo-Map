ae307ba24d3e28cf8afea521f56dda47866f8f5d
evenly-epic-mule <evenly-epic-mule@trash-mail.com>
2016-11-07 21:57:04 +0000

    Update models.py

--- pogom/models.py
+++ pogom/models.py
@@ -350,9 +350,9 @@ class Pokemon(BaseModel):
                              (Pokemon.disappear_time > (datetime.now() - timedelta(days=1)))
                              ))
         # Sqlite doesn't support distinct on columns
-        #if args.db_type == 'mysql':
+        # if args.db_type == 'mysql':
         #    query = query.distinct(Pokemon.spawnpoint_id)
-        #else:
+        # else:
         # mysql is f**king this up for some reason (2.6k => 400 spawnpoints found)
         query = query.group_by(Pokemon.spawnpoint_id)
 
